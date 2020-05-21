59836d27572861756e56934f5047faedd2ff68ab
hungmuha <hung.soboss@gmail.com>
2020-05-21 16:34:02 -0600

    added functionality to add task in list

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React,{useState} from 'react';
 import CreateTodo from './components/CreateTodo';
 import TodoList from './components/TodoList';
 import './App.css';
 
-const tasks = [
-  {
-    title: 'cook',
-    detail: 'use knife to cut stuff',
-    status: 1,
-    taskID: 1,
-  },
-  {
-    title: 'clean house',
-    detail: 'use gasoline',
-    status: 0,
-    taskID: 0,
-  },
-];
+const App = () => {
+  const Task = function(title) {
+    let randomNum = Math.floor(Math.random()*1000*7).toString();
+  
+    this.title = title;
+    this.status = 0;
+    this.taskID = randomNum.concat('-',Date.now());
+  }
+  const TaskList = [
+    {
+      title: 'cook',
+      status: 1,
+      taskID: 1,
+    },
+    {
+      title: 'clean house',
+      status: 0,
+      taskID: 0,
+    },
+  ];
 
-const[newTask,setNewTask] = React.useState('');
-const handleNewTaskChange = event => {
-  setNewTask(event.target.value);
-};
+  const [newInput, setNewInput] = useState('');
+  const [tasks,setTasks] = useState(TaskList);
+  const handleInputChange = event => {
+    setNewInput(event.target.value);
+  };
+  const handleNewtaskSubmit = event => {
+    let newTask = new Task(newInput);
+    setTasks([...TaskList,newTask]);
+    event.preventDefault();
+  }
 
 
-function App() {
   return (
     <div className="container">
       <TodoList list={tasks}/>
-      <CreateTodo inputChange={handleNewTaskChange}/>
+      <CreateTodo 
+        inputValue={newInput} 
+        inputChange={handleInputChange} 
+        submitNewTask={handleNewtaskSubmit}
+      />
     </div>
   );
 };
--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const CreateTodo = ({inputChange}) => {
+const CreateTodo = ({inputChange,inputValue,submitNewTask}) => {
     return(
         <div>
             <h1>here is the form</h1>
-            <form onSubmit>
+            <form onSubmit={submitNewTask}>
                 <label htmlFor="title" >Title: </label>
-                <input id="title" type="text" onChange={inputChange}></input>
+                <input 
+                    id="title" 
+                    type="text" 
+                    onChange={inputChange}
+                    value={inputValue}
+                />
+                <button type="submit" disabled = {!inputValue}>
+                    Submit
+                </button>
             </form>
         </div>
     )
